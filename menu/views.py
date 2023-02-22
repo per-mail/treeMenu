6db499e94b5752b9d139bbbbd86f2fc58afb957b
nikolay <per-mail@mail.ru>
2023-02-22 13:48:16 +0700

    21/02/2023

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .models import Rubric
 
-# создаём многоуровневое меню https://www.youtube.com/watch?v=DIS6e_oZOQA&list=PLmC7X4gkQWCeYJeFQID3m1f5I6SZgH9Vv&index=18&t=181s
 def testmenu(request):
-    return render(request, 'menu/testmenu.html', {'rubrics': Rubric.objects.all()})
+    return render(request, 'menu/testmenu.html')
 
 def get_rubric(request):
     pass
