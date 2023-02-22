b3959d692d667aec8f0d6a5eeb1e2985c73a7ee2
nikolay <per-mail@mail.ru>
2023-02-22 14:29:58 +0700

    21/02/2023

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Rubric
+
 
 def testmenu(request):
-    return render(request, 'menu/testmenu.html')
+    return render(request, 'menu/menu.html')
 
 def get_rubric(request):
     pass
